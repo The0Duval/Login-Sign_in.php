86535caea6f32a78bf7ce9a654679e085b19ef00
Theo Duval <theoduval@macbook-pro-de-theo.home>
2022-04-20 11:47:54 +0200

    login page

---
+++ php/login.php
@@ -0,0 +1,30 @@
+<?php
+ob_start();
+
+$title = "login";
+
+if("POST" === $_SERVER ["REQUEST_METHOD"]){
+    require_once __DIR__ ."/../data/pdo.php/";
+    $mail = filter_input(INPUT_POST, "mail");
+    $mdp = hash("sha512", filter_input("mdp"));
+    $maRequete = $pdo -> prepare("SELECT `id`, `mail`, `mdp` FROM `user` WHERE `mail` = :mail;");
+    $maRequete -> execute(
+        [":mail;" => $mail]
+    );
+    $user = $maRequete->fetch();
+    
+    if(!$user || $user['mdp'] !== $mdp ){
+        $message = "Utilisateur introuvable";
+        http_response_code(403);
+        require_once __DIR__ . "/../html/alert/404.php";
+    }else{
+        $_SESSION["user"] = $user;
+        header("Location /home");
+        exit();
+    }
+}   
+
+
+require_once __DIR__ . "/../hmtl/login.php";
+
+$content = ob_get_clean();
